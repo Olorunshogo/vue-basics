d53314d35383881912f62ad522d8f55c0ccb39a9
BAMTEFA Olorunshogo Moses <shownzy001@gmail.com>
2025-03-11 04:59:22 +0100

    Event Handling, Conditional and List Rendering

--- vue-project/src/components/TestEssential.vue
+++ vue-project/src/components/TestEssential.vue
@@ -1,7 +1,9 @@
 
 <script lang="ts" setup>
-  import { ref } from 'vue';
+  import { ref, reactive, computed, defineProps } from 'vue';
+  // import TodoItem from './TodoItem.vue';
 
+  // Template Syntax, Class & Style Bindings
   const title = ref<string>('Attribute Binding');
   const isRed = ref<boolean>(true);
   const color = ref<string>('green');
@@ -13,9 +15,82 @@
   function toggleColor() {
     color.value = color.value === 'green' ? 'blue' : 'red';
   }
-  function toggleDisable() {
-    isButtonDisabled.value = !isButtonDisabled.value
+  const isActive = ref(true);
+  function toggleActive() {
+    isActive.value = !isActive.value
   }
+  // const activeColor = ref('red');
+  // const fontSize = ref(20);
+  const styleObject = ref({
+    color: 'red',
+    fontSize: '30px'
+  })
+
+
+  // Computed Properties
+  const author = reactive({
+    name: 'John Author',
+    books: [
+      'Vue 2 - Advanced Guide',
+      'Vue 3 - Basic Guide',
+      'Vue 4 - The Mystery'
+    ]
+  });
+  const publishedBooksMessage = computed(() => {
+    return author.books.length > 0 ? 'Yes' : 'No';
+  });
+  const firstName = ref('John');
+  const lastName = ref('Author');
+  const fullName = computed({
+    get() {
+      return firstName.value + ' ' + lastName.value;
+    },
+    set(newValue) {
+      [firstName.value, lastName.value] = newValue.split(' ');
+    }
+  })
+
+  // Conditional and List Rendering
+  const vIf = ref(true);
+  const toggleVif = ref(true);
+  const vShow = ref(true);
+  const parentMessage = ref('Parent');
+  const items = ref([
+    { message: 'Item 1' },
+    { message: 'Item 2' }
+  ]);
+  const myObject = reactive({
+    title: 'How to do lists in Vue',
+    author: 'Jane Doe',
+    publishedAt: '2016-04-10'
+  })
+
+  let id = 0;
+  const newTodo = ref('');
+  const hideCompleted = ref(false)
+  const todos = ref([
+  { id: id++, text: 'Learn HTML', done: true },
+  { id: id++, text: 'Learn JavaScript', done: true },
+  { id: id++, text: 'Learn Vue', done: false }
+])
+  let newTodoId = 4;
+  function addTodo() {
+    todos.value.push({
+      id: id++,
+      text: newTodo.value,
+      done: false
+    })
+    newTodo.value = '';
+  }
+
+  function removeTodo(todo) {
+    todos.value = todos.value.filter((t) => t !== todo)
+  }
+  function shout(warning: string) {
+    alert(warning)
+  }
+
+
 
 </script>
 
@@ -38,12 +113,72 @@
         This should be green, and should toggle between red and blue on click.
         <a :href="googleLink" target="_blank">Google</a>
       </p>
+      <p :class="{ active: isActive }" @click="toggleActive">Active or not - Click to toggle color</p>
+      <!-- <p :style="{ color: activeColor, fontSize: fontSize + 'px' }">Active Color and Font Size</p> -->
+      <p :style="styleObject">Active Color and Font Size</p>
 
-      <button :disabled="isButtonDisabled" @click="toggleDisable">Button</button>
+      <button :disabled="isButtonDisabled" @click="isButtonDisabled = !isButtonDisabled">Button</button>
       <form action="" @submit.prevent="onSubmit"></form>
     </div>
 
-    
+    <!-- Computed Property -->
+    <div>
+      <h2>Computed Property</h2>
+      <p>Has author published books:
+        <span>{{ publishedBooksMessage }}</span>
+      </p>
+      <p>Full name of the author is: {{ fullName }}</p>
+
+    </div>
+
+    <!-- Conditional and List Rendering -->
+     <div>
+      <h3>Conditional and List Rendering</h3>
+      <p>
+        <span v-if="vIf">Vue is awesome!. V-if is true</span>
+        <span v-else>Oh no 😢, v-if is false</span> &NonBreakingSpace;
+        <button @click="vIf = !vIf"> Toggle V-if</button>
+      </p>
+      <ul>
+        <li v-for="value in items" :key="value">
+          <!-- <li v-for="{item, index} in items" :key="item"> -->
+          <!-- {{ parentMessage }} - {{ index }} - {{ items.message }} -->
+            {{ value.message }}
+        </li>
+      </ul>
+      <ul>
+        <li v-for="{ message } in items" :key="message">
+          {{ message }}
+        </li>
+      </ul>
+      <ul>
+        <li v-for="(value, key, index) in myObject" :key="index">
+          {{ index + 1 }}. {{ key }}:  {{ value }}
+          <span v-for="n in 10" :key="n">{{ n }}</span>
+        </li>
+      </ul>
+      <div>
+        <form @submit.prevent="addTodo">
+          <input v-model="newTodo" required placeholder="new todo">
+          <button>Add Todo</button>
+        </form>
+
+        <ul>
+          <li v-for="todo in todos" :key="todo.id">
+            <input type="checkbox" v-model="todo.done"> &NonBreakingSpace;
+            <span :class="{ done: todo.done }">{{ todo.text }}</span> &NonBreakingSpace;
+            <button @click="removeTodo(todo)">X</button>
+          </li>
+        </ul>
+
+        <button @click="hideCompleted = !hideCompleted">
+          {{ hideCompleted ? 'Show all' : 'Hide completed' }}
+        </button>
+        &NonBreakingSpace;
+        <button @click="shout('Done!!')">Alert done</button>
+      </div>
+
+     </div>
   </div>
 </template>
 
@@ -61,4 +196,13 @@
  .red {
   color: red;
  }
+
+ .active {
+  color: green;
+ }
+
+ .done {
+  text-decoration: line-through;
+ }
+
 </style>
---
+++ vue-project/src/components/TodoItem.vue
@@ -0,0 +1,20 @@
+
+<script lang="ts" setup>
+  defineEmits(['remove']);
+  defineProps(['title']);
+</script>
+
+<template>
+  <div>
+    <ul>
+      <li>
+        <label for="checkbox">
+          <input type="checkbox" name="checkbox" id="checkbox" v-model="todo.done">
+        </label>
+        {{ title }}
+        <button @click="$emit('remove')">Remove</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
